8fc885bbeaf6cb03f3d6acff8e7038eff153f3bf
Angélica M. Vega Aponte <31680125+angelicavega@users.noreply.github.com>
2020-08-09 19:20:06 -0400

    Add files via upload

---
+++ HeartRate.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartRate : MonoBehaviour
+{
+
+    public float alphaLevel = 0.2f;
+    public float redLevel = 1f;
+    public float greenLevel = 1f;
+    public float blueLevel = 1f;
+
+    bool changeBool = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (changeBool == true) {
+            StartCoroutine(HeartRateChange());
+
+            float Achange = Random.Range(-0.0002f, 0.002f);
+            float Rchange = Random.Range(-0.01f, 0.01f);
+            float Gchange = Random.Range(-0.01f, 0.01f);
+            float Bchange = Random.Range(-0.01f, 0.01f);
+
+
+            alphaLevel = alphaLevel + Achange;
+            redLevel = redLevel + Rchange;
+            greenLevel = greenLevel + Gchange;
+            blueLevel = blueLevel + Bchange;
+
+            GetComponent<MeshRenderer>().material.color = new Color(redLevel, greenLevel, blueLevel, alphaLevel);
+        }
+    }
+
+    IEnumerator HeartRateChange()
+    {
+        changeBool = false;
+        yield return new WaitForSeconds(0.01f);
+        changeBool = true;
+
+    }
